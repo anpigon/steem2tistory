9640e5bf7c4f48edbf701b430fb723665bfe2eee
markan <markan@nexl.kr>
2019-06-18 13:25:15 +0900

    .

--- src/services/TistoryService.js
+++ src/services/TistoryService.js
@@ -33,3 +33,29 @@ export const getPostList = ({ blogName, page }) => {
   };
   return client.get(request, { params });
 };
+
+export const addNewPost = ({
+  blogName,
+  title,
+  content,
+  visibility = 0,
+  category = 0,
+  slogan,
+  tag,
+  acceptComment = 1
+}) => {
+  const request = "/apis/post/list";
+  const body = {
+    output: "json",
+    access_token: getAccessToken(),
+    blogName,
+    title,
+    content,
+    visibility,
+    category,
+    slogan,
+    tag,
+    acceptComment
+  };
+  return client.post(request, body);
+};
